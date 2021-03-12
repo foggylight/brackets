c1a5faf55f0aec9edd6437eac88acc494a7210cb
Aleksandra Azarova <alekssaffl@yandex.ru>
2021-03-12 19:05:19 +0300

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const open = [];
+    const close = [];
+    bracketsConfig.forEach((el) => {
+        open.push(el[0]);
+        close.push(el[1]);
+    });
+
+    const arr = str.split("");
+    acc = [];
+
+    for (let el of arr) {
+        if (acc[acc.length - 1] === el) {
+            acc.pop();
+        } else if (open.includes(el)) {
+            const index = open.indexOf(el);
+            acc.push(close[index]);
+        } else if (acc.length === 0) {
+            return false;
+        }
+    };
+
+    return acc.length === 0;
+};
